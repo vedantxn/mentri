4965769fe81581dcf4adfb7192a841af5a7803cc
vedantxn <vn.2006@outlook.com>
2025-10-06 10:57:35 +0530

    Better-auth setup, Authentication Frontend and database setup

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+//"use client";
+
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+    return <SignInView />;
+};
+
+export default Page;
+
+//  https://localhost:3000/sign-in
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,56 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  
+  const { data: session } = authClient.useSession();
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      password,
+      name,
+    }, {
+      onError: (error) => {
+        window.alert("Something went wrong");
+      },
+      onSuccess: () => {
+        window.alert("Success");
+      },
+    });
+  }
+
+  if (session) {
+    return (
+    <div className="flex flex-col p-4 gap-y-4">
+      <p>Logged in as {session.user.name}</p>
+      <Button onClick={() => authClient.signOut()}>Sign out</Button>
+    </div>
+    );
+  }
+     
   return (
-    <div>
-      <h1>Home</h1>
-      <Button className="hover:bg-blue-500">Click me</Button>
+    <div className="flex flex-col p-4 gap-y-4">
+      <Input
+        placeholder="Name" 
+        value={name}
+        onChange={(e) => setName(e.target.value)} />
+      <Input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)} />
+      <Input
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Sign Up</Button>
     </div>
   );
 }
