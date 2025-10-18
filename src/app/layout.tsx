4386331a6c1dc82b0db4573ed60c1c52999c82a1
vedantxn <vn.2006@outlook.com>
2025-10-18 17:28:13 +0530

    trpc setup for both client and server side, preview prefetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { TRPCReactProvider } from "@/trpc/client";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <TRPCReactProvider>
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
+    </TRPCReactProvider>
   );
 }
